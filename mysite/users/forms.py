cb53d94d67469d453d54e60d497b0587d956386a
yeemaae <noobproger@gmail.com>
2024-02-11 02:23:08 +0600

    created & stylized register.html

--- mysite/users/forms.py
+++ mysite/users/forms.py
@@ -4,12 +4,21 @@ from django import forms
 
 
 class NewUserForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-    username = forms.CharField(required=True)
-    password1 = forms.CharField(required=True)
-    password2 = forms.CharField(required=True)
+    email = forms.EmailField(required=True, widget=forms.EmailInput(
+        attrs={"class": "focus:outline-none", "placeholder": "mail@mail.com"}
+    ))
+    username = forms.CharField(required=True, widget=forms.TextInput(
+        attrs={"class": "focus:outline-none", "placeholder": "Enter username"}
+    ))
+    password1 = forms.CharField(required=True, widget=forms.PasswordInput(
+        attrs={"class": "focus:outline-none", "placeholder": "Enter Password"}
+    ))
+    password2 = forms.CharField(required=True, widget=forms.PasswordInput(
+        attrs={"class": "focus:outline-none", "placeholder": "Confirm Password"}
+    ))
 
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
+
 
